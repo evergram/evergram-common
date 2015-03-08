cc86df7c12e4b38410766cce90659cda08cefa18
crimsonronin <josh.stuart@zoopcommerce.com>
2015-03-08 11:02:10 +1100

    Added init triggers

--- index.js
+++ index.js
@@ -9,25 +9,38 @@ var passport = require('passport');
 var config = require('./config/config');
 var utils = require('./lib/utils');
 
+var initApp = true;
+var initModels = true;
+var initMongoose = true;
 var app = express();
 
-// Connect to mongodb
-var connect = function () {
-    var options = {server: {socketOptions: {keepAlive: 1}}};
-    mongoose.connect(config.db, options);
-};
-connect();
 
-mongoose.connection.on('error', console.log);
-mongoose.connection.on('disconnected', connect);
+if (initMongoose) {
+    // Connect to mongodb
+    var connect = function () {
+        var options = {server: {socketOptions: {keepAlive: 1}}};
+        mongoose.connect(config.db, options);
+    };
+    connect();
 
-// Bootstrap models
-fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
-    if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
-});
+    mongoose.connection.on('error', console.log);
+    mongoose.connection.on('disconnected', connect);
+    initMongoose = false;
+}
 
-// Bootstrap application settings
-require('./config/express')(app, passport);
+if (initModels) {
+    // Bootstrap models
+    fs.readdirSync(__dirname + '/app/models').forEach(function (file) {
+        if (~file.indexOf('.js')) require(__dirname + '/app/models/' + file);
+    });
+    initModels = false;
+}
+
+if (initApp) {
+    // Bootstrap application settings
+    require('./config/express')(app, passport);
+    initApp = false;
+}
 
 /**
  * Expose
@@ -35,6 +48,10 @@ require('./config/express')(app, passport);
 
 module.exports = {
     app: app,
+    db: mongoose,
     config: config,
+    models: {
+        user: mongoose.model('User')
+    },
     utils: utils
 };

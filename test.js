c0571ea12aa72f05d5b4894f1e8effc4b4aa190a
crimsonronin <josh.stuart@zoopcommerce.com>
2015-04-06 16:54:03 +1000

    Created the tracking manager. Added some necessary fields so we can track if the user owns the image or not.

--- test.js
+++ test.js
@@ -3,21 +3,26 @@
  */
 
 var common = require('./lib/');
-var image = common.image.manager;
-var utils = common.utils;
+var user = common.models.User;
+var logger = common.utils.logger;
+var userManager = common.user.manager;
+var trackingManager = common.tracking.manager;
 
+//init db
+common.db.connect();
 
-//image.saveFromUrl(
-//'https://igcdn-photos-f-a.akamaihd.net/hphotos-ak-xfa1/t51.2885-15/11032988_799045916811037_763042691_n.jpg',
-//'test'
-//);
+var id = '5520bafafaa4723e80bddced';
+var criteria = {_id: id};
 
-//utils.zipFiles(
-//    [
-//        {
-//            filepath: 'tmp/test.jpg',
-//            name: 'my-file.jpg'
-//        }
-//    ],
-//    'test'
-//);
+userManager.find({criteria: criteria}).then(function (user) {
+    trackingManager.createUser(user).then(function (r) {
+        trackingManager.trackEvent(user, 'Tagged Instagram Photos', {
+            'Username': user.getUsername(),
+            'Total Photos': 10,
+            'Total Own Photos': 2,
+            'Total Other Photos': 8
+        }).then(function () {
+            logger.info('Done')
+        });
+    });
+});

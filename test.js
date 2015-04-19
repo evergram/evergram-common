c987e89a2fce1aa190f4a60b01278164a433a54c
crimsonronin <josh.stuart@zoopcommerce.com>
2015-04-19 15:58:42 +1000

    Added new segment tracking. Created a backfill for tracking. Added period to image set rather than just a date.

--- test.js
+++ test.js
@@ -11,30 +11,40 @@ var trackingManager = common.tracking.manager;
 
 //init db
 common.db.connect();
+
+var id = '553208b7a1f2d86b0206639b';
+var options = {criteria: {_id: id}};
+//var options = {};
+
+//backfill
+userManager.findAll(options).then(function (users) {
+    _.forEach(users, function (user) {
+        logger.info('Start', user.getUsername());
+        trackingManager.createUser(user).
+        then(function (r) {
+            return trackingManager.trackEvent(user, 'Test', {stuff: 'yeah stuff'}, new Date());
+        }).
+        then(function (r) {
+            logger.info('Done', user.getUsername());
+        }).
+        fail(function (err) {
+            logger.error(err);
+        }).done();
+    });
+}).fail(function (err) {
+    logger.error(err);
+}).done();
+
+
+//userManager.find({criteria: {'instagram.username': 'emteds'}}).then(function (user) {
+//    user.jobs.instagram.inQueue = true;
 //
-//var id = '5520bafafaa4723e80bddced';
-//var criteria = {_id: id};
-//
-////backfill
-//
-//userManager.findAll().then(function (users) {
-//    _.forEach(users, function (user) {
-//        trackingManager.createUser(user).then(function (r) {
-//            logger.info(user.getUsername());
-//        });
-//    });
+//    userManager.update(user);
 //});
-
-
-userManager.find({criteria: {'instagram.username': 'emteds'}}).then(function (user) {
-    user.jobs.instagram.inQueue = true;
-
-    userManager.update(user);
-});
-
-var newUser = new User();
-newUser.firstName = 'Test';
-newUser.instagram = {
-    username: 'test'
-};
-userManager.create(newUser);
+//
+//var newUser = new User();
+//newUser.firstName = 'Test';
+//newUser.instagram = {
+//    username: 'test'
+//};
+//userManager.create(newUser);

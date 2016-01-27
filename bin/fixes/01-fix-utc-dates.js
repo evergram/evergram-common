8bd1ee57dc6f6e560546e3f9add7bb3e790471f5
crimsonronin <joshstuartx@gmail.com>
2016-01-27 22:12:32 +1100

    updated the UTC date fixer.

--- bin/fixes/01-fix-utc-dates.js
+++ bin/fixes/01-fix-utc-dates.js
@@ -1,9 +1,9 @@
-/**
- * Module dependencies
- */
+#!/usr/bin/env node
+
 process.env.TZ = 'UTC';
 
 var _ = require('lodash');
+var q = require('q');
 var moment = require('moment-timezone');
 var common = require('../../lib/');
 var logger = common.utils.logger;
@@ -22,33 +22,64 @@ var options = {
 //backfill users
 userManager.findAll(options).
 then(function(users) {
+    var promises = [];
     _.forEach(users, function(user) {
         if (!hasUtcDate(user.signupCompletedOn)) {
+            var promise = q.defer();
+            promises.push(promise.promise);
             var normalizedDate = normalizeDate(user.signupCompletedOn);
             user.signupCompletedOn = normalizedDate;
 
-            console.log(user.instagram.username, normalizedDate.format());
-            user.save();
+            logger.info('User sign up on ' + user.instagram.username + ' : ' + normalizedDate.format());
+            user.save(function() {
+                promise.resolve();
+            });
         }
     });
-});
 
-// image sets
-printManager.findAll().
+    return q.all(promises);
+}).
+then(function() {
+    logger.info('Get all image sets');
+    return printManager.findAll();
+}).
 then(function(imageSets) {
+    var promises = [];
+    logger.info('Found ' + imageSets.length + ' image sets');
     _.forEach(imageSets, function(imageSet) {
+        var updated = false;
+        var promise;
+
         if (!hasUtcDate(imageSet.startDate)) {
-            imageSet.startDate = normalizeDate(imageSet.startDate);
-            logger.info(imageSet.user.instagram.username);
-            logger.info(imageSet.startDate);
+            var startDate = normalizeDate(imageSet.startDate);
+            imageSet.startDate = startDate;
+            logger.info('User ' + imageSet.user.instagram.username);
+            logger.info('Image set period ' + imageSet.period);
+            logger.info('Start date: ' + startDate.format());
+            updated = true;
         }
 
         if (!hasUtcDate(imageSet.endDate)) {
-            imageSet.endDate = normalizeDate(imageSet.endDate);
+            var endDate = normalizeDate(imageSet.endDate);
+            imageSet.endDate = endDate;
+            logger.info('End date: ' + endDate.format());
+            updated = true;
         }
 
-        imageSet.save();
+        if (updated) {
+            promise = q.defer();
+            imageSet.save(function() {
+                promise.resolve();
+            });
+            promises.push(promise.promise);
+        }
     });
+
+    return q.all(promises);
+}).
+then(function() {
+    logger.info('Updates complete');
+    process.exit(0);
 });
 
 /**

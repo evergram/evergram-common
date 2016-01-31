9ee45f744a8697b36f4fda9dd06444d72bf7d7ac
Richard O'Brien <richard@stichmade.com>
2016-01-31 21:32:41 +1100

    Various changes:
    1. Script added to `./bin/fixes` to fix data types. Fixes type for users with the following fields/data types in the Users collection.
    - `address.postcode`: From `Number` to `String` (39 effected users)
    - `instagram.id`: From `Number` to `String` (32 effected users)
    - `createdOn`: From `String` to `Date` (39 effected users)
    - `signupCompletedOn`: From `String` to `Date` (15 effected users)
    
    2. Fixed Instagram Mapper: Instagram User Mapper was incorrectly mapping `profile_picture`, `website`, and `bio` fields.
    
    3. Created associated mocha test for Instagram User mapper.
    
    4. Added script to update Stripe users meta data so that all users have consistent metadata field.

---
+++ bin/resync-stripe-user-metadata.js
@@ -0,0 +1,98 @@
+/**
+ * Module dependencies
+ */
+process.env.TZ = 'UTC';
+
+var q = require('q');
+var _ = require('lodash');
+var common = require('../lib/');
+var logger = common.utils.logger;
+var userManager = common.user.manager;
+var config = common.config.stripe;
+var stripe = require('stripe');
+
+//init db
+common.db.connect();
+
+var stripe = stripe("sk_test_KN8z6UJtLbBWITp7FZUGiWKI");
+
+var options = {
+    criteria: {
+        active: true,
+        signupComplete: true,
+        email: {
+            $exists: true,
+            $ne: ''
+        },
+        "billing.stripeId": {
+            $exists: true,
+            $ne: ''
+        }
+    }
+};
+
+
+//backfill
+userManager.findAll(options).
+then(function(users) {
+    var promises = [];
+    var i = 1;
+    var done_i = 1;
+    var total = users.length;
+
+    _.forEach(users, function(user) {
+        logger.info('------------------------ STARTING USER ' + user.instagram.username + ', ' + i + ' of ' + total);
+        var promise = q.defer();
+        promises.push(promise.promise);
+
+        // update stripe user metadata
+        updateCustomer(user.billing.stripeId, {
+            metadata: {
+                first_name: user.firstName,
+                instagram_id: user.instagram.id,
+                instagram_username: user.instagram.username,
+                last_name: user.lastName,
+                pixy_customer_id: user._id.toString()
+            }
+        }).
+        then(function() {
+            logger.info('Updated user in Stripe');
+            logger.info('------------------------ COMPLETED USER ' + user.instagram.username + ', ' + done_i + ' of ' + total);
+            done_i++;
+            promise.resolve();
+        }).
+        fail(function(err) {
+            logger.info('------------------------ FAILED USER ' + user.instagram.username + ', ' + done_i + ' of ' + total);
+            logger.error(err);
+            promise.resolve();
+        });
+
+        i++;
+    });
+
+    return q.all(promises);
+}).
+then(function() {
+    logger.info('WE ARE DONE!');
+    logger.info('Waiting 10s before closing...');
+
+    // wait 10s before closing
+    setTimeout(function() {
+        process.exit(0);
+    }, 10000);
+});
+
+
+/**
+ * Updates an existing customer in stripe.
+ *
+ * @param id is the stripe customer id.
+ * @param data is the customer data to be updated.
+ * @returns {promise|*|q.promise}
+ */
+function updateCustomer(id, data) {
+    logger.info('updateCustomer + ' + id + ', data=' + data);
+    return q.ninvoke(stripe.customers, 'update', id, data);
+};
+
+
---
+++ test/specs/instagram-user-mapper-spec.js
@@ -0,0 +1,151 @@
+/**
+ * @author Richard O'Brien <richard@printwithpixy.com>.
+ */
+var should = require('should');
+var sinon = require('sinon');
+var q = require('q');
+var moment = require('moment');
+var db = require('../../lib/db');
+var userManager = require('../../lib/user/manager');
+var instagramUser = require('../../lib/mapper/instagramUser');
+
+describe('Map Instagram User profile to Pixy User model', function() {
+
+    var data;
+
+    before(function(done) {
+        db.connect().
+        then(function() {
+            done();
+        });
+    });
+
+    beforeEach(function(done) {
+        
+        data = {
+            "authToken" : "1574083.2fd0517.1097afd5d55543e3bbbf7003c49fe3d6",
+            "_json" : {
+                "data" : {
+                    "id": "1574083",
+                    "username": "snoopdogg",
+                    "full_name": "Snoop Dogg",
+                    "profile_picture": "http://distillery.s3.amazonaws.com/profiles/profile_1574083_75sq_1295469061.jpg",
+                    "bio": "This is my bio",
+                    "website": "http://snoopdogg.com",
+                    "counts": {
+                        "media": 1320,
+                        "follows": 420,
+                        "followed_by": 3410
+                    }
+                }
+            }
+        };
+
+        done();
+    });
+
+    afterEach(function(done) {
+
+        q.all([]).
+        then(function() {
+            done();
+        });
+    });
+
+    it('should create a user when no user is passed', function(done) {
+        const user = {};
+
+        var newUser = instagramUser.toModel(data, user);
+        
+        should.exist(newUser);
+
+        newUser.firstName.should.be.eql("Snoop");
+        newUser.lastName.should.be.eql("Dogg");
+        newUser.instagram.id.should.be.eql("1574083");
+        newUser.instagram.username.should.be.eql("snoopdogg");
+        newUser.instagram.authToken.should.be.eql("1574083.2fd0517.1097afd5d55543e3bbbf7003c49fe3d6");
+        newUser.instagram.follows.should.be.eql(420);
+        newUser.instagram.followers.should.be.eql(3410);
+        newUser.instagram.media.should.be.eql(1320);
+        newUser.instagram.profilePicture.should.be.eql("http://distillery.s3.amazonaws.com/profiles/profile_1574083_75sq_1295469061.jpg");
+        newUser.instagram.website.should.be.eql("http://snoopdogg.com");
+        newUser.instagram.bio.should.be.eql("This is my bio");
+
+        done();
+    });
+
+    it('should update user when a user is passed', function(done) {
+        const user = {
+            "_id" : 'ObjectId("5694cc3daa7774c966202e5d")',
+            "updatedOn" : 'ISODate("2016-01-20T06:22:58.000Z")',
+            "createdOn" : 'ISODate("2016-01-11T13:00:00.000Z")',
+            "signupComplete" : true,
+            "active" : true,
+            "timezone" : "Australia/Melbourne",
+            "instagram" : {
+                "id" : "227081463",
+                "username" : "rikileela",
+                "authToken" : "227081463.2fd0517.df8d134ca4ec48ffaf74169d2d95551f",
+                "follows" : 190,
+                "followers" : 45,
+                "media" : 154
+            },
+            "jobs" : {
+                "instagram" : {
+                    "inQueue" : true,
+                    "lastRunOn" : 'ISODate("2016-01-20T06:07:49.134Z")',
+                    "nextRunOn" : 'ISODate("2016-01-20T06:22:49.134Z")'
+                }
+            },
+            "payments" : [],
+            "billing" : {
+                "option" : "GIFT3",
+                "stripeId" : "cus_7hoRRwvDHwcJJm"
+            },
+            "address" : {
+                "country" : "Australia",
+                "postcode" : "3556",
+                "state" : "vic",
+                "suburb" : "Eaglehawk",
+                "line1" : "59 Church St"
+            },
+            "email" : "rlbrow@hotmail.com",
+            "lastName" : "Coates",
+            "firstName" : "Riki",
+            "__v" : 0,
+            "signupCompletedOn" : 'ISODate("2016-01-11T13:00:00.000Z")'
+        };
+
+        var newUser = instagramUser.toModel(data, user);
+        
+        should.exist(newUser);
+
+        newUser.firstName.should.be.eql("Snoop");
+        newUser.lastName.should.be.eql("Dogg");
+        newUser.instagram.id.should.be.eql("1574083");
+        newUser.instagram.username.should.be.eql("snoopdogg");
+        newUser.instagram.authToken.should.be.eql("1574083.2fd0517.1097afd5d55543e3bbbf7003c49fe3d6");
+        newUser.instagram.follows.should.be.eql(420);
+        newUser.instagram.followers.should.be.eql(3410);
+        newUser.instagram.media.should.be.eql(1320);
+        newUser.instagram.profilePicture.should.be.eql("http://distillery.s3.amazonaws.com/profiles/profile_1574083_75sq_1295469061.jpg");
+        newUser.instagram.website.should.be.eql("http://snoopdogg.com");
+        newUser.instagram.bio.should.be.eql("This is my bio");
+
+        done();
+    });
+
+    it('should split full name across firstname and lastname fields', function(done) {
+        const user = {};
+
+        var newUser = instagramUser.toModel(data, user);
+        
+        should.exist(newUser);
+
+        newUser.firstName.should.not.be.eql("Snoop Dogg");
+        newUser.lastName.should.not.be.eql("Snoop Dogg");
+
+        done();
+    });
+
+});
